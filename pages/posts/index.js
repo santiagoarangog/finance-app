030e9ed0687a71ad85b06a0c7cc34486cfe9fc6a
Santiago Arango Gutierrez <santiago.arango@experimentality.co>
2022-04-17 15:45:14 -0500

    Add serve side for posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,11 +1,39 @@
+import React from "react";
+import Link from "next/link";
+
 import Title from "../../components/title";
 import Layout from "../../components/layout";
 
-export default function Posts() {
+export default function Posts({posts}) {
+
     return (
         <Layout>
             <Title>Posts Page</Title>
+            <div>
+                {
+                    posts.map(post => {
+                        return (
+                            <Link href={'/posts/[id]'} as={`/posts/${post.id}`}>
+                                <a>
+                                    <h3>{post.title}</h3>
+                                    <p>{post.body}</p>
+                                </a>
+                            </Link>
+                        )
+                    })
+                }
+            </div>
             <p>Nuestra página de posts</p>
         </Layout>
     )
+}
+
+export async function getServerSideProps() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const posts = await res.json()
+    return {
+        props: {
+            posts
+        }
+    };
 }
